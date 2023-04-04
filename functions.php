a8650c70d9dc249a73b024fe4b05bd0c528621c2
Brent <bmiller180787@gmail.com>
2023-04-04 14:29:24 +0100

    wireframes and testing

--- functions.php
+++ functions.php
@@ -2,13 +2,17 @@
 
 function returnHtmlFromArray(array $trees): string
 {
-    $htmlbody = '';
-    foreach ($trees as $tree) {
-        $htmlbody .= '<div class="card">'
-            . '<div><img src="' . $tree->getImageLink() . '" alt="A ' . $tree->getName() . '"></div>'
-            . '<h2>' . $tree->getName() . '</h2>'
-            . '<p>' . $tree->getScientificName() . '</p>'
-            . '</div>';
+    try {
+        $htmlbody = '';
+        foreach ($trees as $tree) {
+            $htmlbody .= '<div class="card">'
+                . '<div><img src="' . $tree->getImageLink() . '" alt="A ' . $tree->getName() . '"></div>'
+                . '<h2>' . $tree->getName() . '</h2>'
+                . '<p>' . $tree->getScientificName() . '</p>'
+                . '</div>';
+        }
+    } catch(Error $exception) {
+        return 'Incorrect Data Supplied';
     }
 
     return $htmlbody;
